7737f27813e93dacf4c5c8af0f79955bd1923bcd
rezasafavi <reza.safavi1371@gmail.com>
2023-01-01 23:27:21 +0330

    new change

---
+++ Project/Letter_Game/Letter-Game.py
@@ -0,0 +1,12 @@
+import random
+
+# make a list of words
+words = ['apple' , 'bannana' , 'orange' , "coconut" , "strawberry" , "lime" ,"grapefruit" , "lemon" , "kumquat" , "blueberry","melon"]
+
+while True:
+    
+# pick a random word
+# draw space for guessing letter
+# take a guess 
+# draw guess letter and strikes
+# print out win/loss
